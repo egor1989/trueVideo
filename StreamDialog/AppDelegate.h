ecd50d82c67c2e1031664eb61a2bfa3d5f233e43
mike <vakulenkom@gmail.com>
2012-07-24 14:43:04 +0400

    Initial Commit

---
+++ StreamDialog/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  StreamDialog
+//
+//  Created by Mike on 24.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ StreamDialog/DetailViewController.h
@@ -0,0 +1,17 @@
+//
+//  DetailViewController.h
+//  StreamDialog
+//
+//  Created by Mike on 24.07.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@property (strong, nonatomic) id detailItem;
+
+@property (strong, nonatomic) IBOutlet UILabel *detailDescriptionLabel;
+
+@end
